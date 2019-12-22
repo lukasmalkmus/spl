85b0f554a6d2a1d007e0d44be2a16dec42f4d3a1
Lukas Malkmus <mail@lukasmalkmus.com>
2019-12-22 04:26:50 +0100

    Revert "Fix CI"
    This reverts commit 0143b19b08b7ec90873e87b124ee59ba74b1dc27.

--- internal/app/spl/constant/doc.go
+++
@@ -1,3 +0,0 @@
-// Package constant implements Values representing untyped SPL constants and
-// their corresponding operations.
-package constant
--- internal/app/spl/types/doc.go
+++
@@ -1,3 +0,0 @@
-// Package types declares the data types and implements the algorithms for
-// type-checking of SPL programs.
-package types
